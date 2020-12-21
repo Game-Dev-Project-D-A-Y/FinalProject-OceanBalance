d1139614e2c554afea13abb7e082b7429be7e8d2
doviec <doviechitiz@gmail.com>
2020-12-22 00:11:49 +0200

    added time & score

---
+++ Assets/BottelsCollectedTextScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/**
+ * This component should be attached to a TextMeshPro object.
+ * It allows to feed an integer number to the text field.
+ */
+[RequireComponent(typeof(TextMeshPro))]
+public class BottelsCollectedTextScript : MonoBehaviour
+{
+
+    private TextMeshPro textField;
+    private int BottelsCollected;
+
+    void Start()
+    {
+        textField = GetComponent<TextMeshPro>();
+    }
+
+    public int GetSBottelsCollected()
+    {
+        return this.BottelsCollected;
+    }
+
+    public void SetBottelsCollectede(int newBottelsCollected)
+    {
+        this.BottelsCollected = newBottelsCollected;
+        this.textField.text = newBottelsCollected.ToString();
+    }
+}
+
+
---
+++ Assets/BottleTimerTextScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+[RequireComponent(typeof(TextMeshPro))]
+
+public class BottleTimerTextScript : MonoBehaviour
+{
+    [Tooltip("Timer of Bottle until they disepear")]
+    [SerializeField] float BottleTime;
+    private TextMeshPro textField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        public int GetBottleTime()
+        {
+            return this.BottleTime;
+        }
+
+        public void SetBottleTime(int newBottleTime)
+        {
+            this.BottleTime = newBottleTime;
+            this.textField.text = newBottleTime.ToString();
+        }
+    }
+}
