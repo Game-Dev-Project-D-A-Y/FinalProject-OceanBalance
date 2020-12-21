148e9ce877f22a0dbb6bc736a645a05e3eb9f07d
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-12-21 23:05:19 +0200

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] int gameScore;         // object that response on score Dovie
+
+    [SerializeField] int bottleTime;        // object that response on bottle time Dovie
+
+    [SerializeField] int scoreToreduceTimeBottle; // check score and update time
+    [SerializeField] int levelScoreToAdd; // check score and update time
+
+ 
+
+    // yishay
+    [SerializeField] GameObject prefabToSpawn;
+    [SerializeField] GameObject baseObject;
+    [SerializeField] int minTimeToCollectBottle
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +32,43 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+
+    public void StartBottleNewTime()
+    {
+        // object that response on bottle time Dovie
+
+
+
+    }
+
+    public  CheckAndUpdateBottleTime()  
+    {
+        // object that response on score Dovie   
+        if(gameScore >= scoreToreduceTimeBottle )
+        {
+           scoreToreduceTimeBottle += levelScoreToAdd;  // for example levelScoreToAdd=10 so every 10 bottles time -- 
+           if(bottleTime == minTimeToCollectBottle){
+               return;
+           }
+           return;
+    }
+
+    public void BottleSpawnerOnCollision()
+    {
+        Vector3 randomPosition = new Vector3();
+        randomPosition = new Vector3(Random.Range(baseObject.transform.localScale.x, baseObject.transform.localScale.z), 0,
+             Random.Range(baseObject.transform.localScale.x, baseObject.transform.localScale.z));
+        GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, Quaternion.identity, baseObject.transform);
+        
+    }
+
+        public void BottleSpawnerOnTimeOut()
+    {
+        Vector3 randomPosition = new Vector3();
+        randomPosition = new Vector3(Random.Range(baseSize.localScale.x, baseSize.localScale.z), 0, Random.Range(baseSize.localScale.x, baseSize.localScale.z));
+        GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, Quaternion.identity, baseObject.transform);
+        
+    }
 }
+
