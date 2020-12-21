6359c035a31368f46d24d00e929bbba592740ccc
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-12-21 16:40:03 +0200

    bottleadded

---
+++ Assets/BottleDestroyonTime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+* This script represents the ball moves and collides.
+*/
+public class BottleDestroyonTime : MonoBehaviour
+{
+
+    public float timeBottleToBeShown;
+   
+   //public GameManager gameManager;
+
+    public GameObject objectToDetroy;
+
+    private bool inPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inPlay = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.deltaTime > timeBottleToBeShown){
+            Debug.Log("bottle destroy"+ this);
+               Destroy(objectToDetroy);
+        }
+
+    }
+
+
+}
+
