6c3f7a7056cebd3a652660bdd11fc78d90d3ddbf
alonp13 <alonp13@gmail.com>
2020-12-22 00:23:59 +0200

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,83 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] TextMeshPro gameScore;         // object that response on score Dovie
+    [SerializeField]
+    TextMeshPro gameScore; // object that response on score Dovie
 
-    [SerializeField] TextMeshPro bottleTime;        // object that response on bottle time Dovie
+    [SerializeField]
+    TextMeshPro bottleTime; // object that response on bottle time Dovie
 
-    [SerializeField] int scoreToreduceTimeBottle; // check score and update time
-    [SerializeField] int levelScoreToAdd; // check score and update time
+    [SerializeField]
+    int scoreToreduceTimeBottle; // check score and update time
 
- 
+    [SerializeField]
+    int levelScoreToAdd; // check score and update time
 
     // yishay
-    [SerializeField] GameObject prefabToSpawn;
-    [SerializeField] GameObject baseObject;
-    [SerializeField] int minTimeToCollectBottle;
+    [SerializeField]
+    GameObject prefabToSpawn;
 
+    [SerializeField]
+    GameObject baseObject;
 
-
+    [SerializeField]
+    int minTimeToCollectBottle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void StartBottleNewTime()
     {
         // object that response on bottle time Dovie
-
-
-
     }
 
-    public void CheckAndUpdateBottleTime()  
+    public void CheckAndUpdateBottleTime()
     {
-        // object that response on score Dovie   
-        if(gameScore >= scoreToreduceTimeBottle )
-        {
-           scoreToreduceTimeBottle += levelScoreToAdd;  // for example levelScoreToAdd=10 so every 10 bottles time -- 
-           if(bottleTime == minTimeToCollectBottle){
-               return;
-           }
-           return;
+        // TODO
     }
-    }
+    private static float num = 1;
     public void BottleSpawnerOnCollision()
     {
-        Vector3 randomPosition = new Vector3();
-        randomPosition = new Vector3(Random.Range(baseObject.transform.localScale.x, baseObject.transform.localScale.z), 0,
-             Random.Range(baseObject.transform.localScale.x, baseObject.transform.localScale.z));
-        GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, Quaternion.identity, baseObject.transform);
-        
+        Debug.Log("BottleSpawnerOnCollision");
+
+        float scaleX = baseObject.transform.localScale.x / 2f;
+        float scaleZ = baseObject.transform.localScale.z / 2f;
+        float randomX = Random.Range(-scaleX,scaleX);
+        float randomZ = Random.Range(-scaleZ,scaleZ);
+        Vector3 randomPosition = new Vector3(randomX,0,randomZ);
+
+        GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, baseObject.transform.localRotation);
+        newObject.transform.parent = baseObject.transform;
+        newObject.transform.localPosition = new Vector3(newObject.transform.localPosition.x,0,newObject.transform.localPosition.z);
     }
 
-        public void BottleSpawnerOnTimeOut()
+    public void BottleSpawnerOnTimeOut()
     {
-        Vector3 randomPosition = new Vector3();
-        randomPosition = new Vector3(Random.Range(baseObject.transform.localScale.x, baseObject.transform.localScale.z), 0,
-             Random.Range(baseObject.transform.localScale.x, baseObject.transform.localScale.z));
-                     GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, Quaternion.identity, baseObject.transform);
+                Debug.Log("BottleSpawnerOnTimeOut");
+        Vector3 randomPosition =
+            new Vector3(Random.Range(-10.0f, 10.0f),
+                0,
+                Random.Range(-10.0f, 10.0f));
+        GameObject newObject =
+            Instantiate(prefabToSpawn.gameObject,
+            randomPosition,
+            Quaternion.identity);
     }
 
     public void OnBottlePicked(GameObject bottle)
     {
         BottleSpawnerOnCollision();
-        Destroy(bottle);
+        Destroy (bottle);
         Debug.Log("OnBottlePicked");
     }
-
-
 }
-
-    
