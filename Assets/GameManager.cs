a5e10ee59a573edb07845abf8d162770d24b33be
alonp13 <alonp13@gmail.com>
2020-12-22 16:34:26 +0200

    create black holes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -32,6 +32,9 @@ public class GameManager : MonoBehaviour
     GameObject prefabToSpawn;
 
     [SerializeField]
+    GameObject blackHoleToSpawn;
+
+    [SerializeField]
     GameObject baseObject;
 
     [SerializeField]
@@ -54,56 +57,7 @@ public class GameManager : MonoBehaviour
         BottleTime();
     }
 
-    public void UpdateGameTime() //update time of game
-    {
-        if (isActive)
-        {
-            timeGained += Time.deltaTime;
-            gameTimeScoreTxt.SetText("Time: {0}", timeGained);
-        }
-    }
-
-    public void CheckAndUpdateBottleTime()
-    {
-        if (
-            bottelsCollected >= scoreToreduceTimeBottle &&
-            minTimeToCollectBottle >= 4
-        )
-        {
-            minTimeToCollectBottle -= 1;
-            scoreToreduceTimeBottle *= 2;
-        }
-        timeLeftToCollectBottle = minTimeToCollectBottle;
-    }
-
-    public void BottleTime()
-    {
-        timeLeftToCollectBottle -= Time.deltaTime;
-        bottleTimerTxt.SetText("Bottle Timer: {0}", timeLeftToCollectBottle);
-        if (timeLeftToCollectBottle <= 0)
-        {
-            timeLeftToCollectBottle = minTimeToCollectBottle;
-
-            Destroy(GameObject.Find("BottlePrefab(Clone)"));
-            SpawnBottle();
-        }
-    }
-
-    public void SpawnBottle()
-    {
-        Debug.Log("SpawnBottle");
-
-        float scaleX = baseObject.transform.localScale.x / 2f;
-        float scaleZ = baseObject.transform.localScale.z / 2f;
-        float randomX = Random.Range(-scaleX, scaleX);
-        float randomZ = Random.Range(-scaleZ, scaleZ);
-        Vector3 randomPosition = new Vector3(randomX, 0, randomZ);
-
-        GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, baseObject.transform.localRotation);
-        newObject.transform.parent = baseObject.transform;
-        newObject.transform.localPosition = new Vector3(newObject.transform.localPosition.x, 0, newObject.transform.localPosition.z);
-    }
-
+    // PUBLIC METHODS
     public void OnBottlePicked(GameObject bottle)
     {
         Debug.Log("OnBottlePicked");
@@ -120,5 +74,71 @@ public class GameManager : MonoBehaviour
         Destroy (ball);
         isActive = false;
         Debug.Log("OnBorderHit");
+    }
+
+    public void OnBlackHole(GameObject ball)
+    {
+        Destroy (ball);
+        isActive = false;
+        Debug.Log("OnBlackHole");
+    }
+
+    // PRIVATE METHODS
+    private void UpdateGameTime() //update time of game
+    {
+        if (isActive)
+        {
+            timeGained += Time.deltaTime;
+            gameTimeScoreTxt.SetText("Time: {0}", timeGained);
+        }
+    }
+
+    private void CheckAndUpdateBottleTime()
+    {
+        if (
+            bottelsCollected >= scoreToreduceTimeBottle &&
+            minTimeToCollectBottle >= 4
+        )
+        {
+            minTimeToCollectBottle -= 1;
+            scoreToreduceTimeBottle *= 2;
+        }
+        timeLeftToCollectBottle = minTimeToCollectBottle;
+    }
+
+    private void BottleTime()
+    {
+        timeLeftToCollectBottle -= Time.deltaTime;
+        bottleTimerTxt.SetText("Bottle Timer: {0}", timeLeftToCollectBottle);
+        if (timeLeftToCollectBottle <= 0)
+        {
+            timeLeftToCollectBottle = minTimeToCollectBottle;
+            GameObject bottleObject = GameObject.Find("BottlePrefab(Clone)");
+            Destroy(bottleObject);
+            CreateBlackHole(bottleObject);
+            SpawnBottle();
+        }
+    }
+
+    private void CreateBlackHole(GameObject bottleObject)
+    {
+        GameObject newObject = Instantiate(blackHoleToSpawn.gameObject, bottleObject.transform.position, baseObject.transform.localRotation);
+        newObject.transform.parent = baseObject.transform;
+        newObject.transform.localPosition = new Vector3(newObject.transform.localPosition.x, 0.6f, newObject.transform.localPosition.z);
+    }
+
+    private void SpawnBottle()
+    {
+        Debug.Log("SpawnBottle");
+
+        float scaleX = (baseObject.transform.localScale.x / 2f) - 2;
+        float scaleZ = (baseObject.transform.localScale.z / 2f) - 2;
+        float randomX = Random.Range(-scaleX, scaleX);
+        float randomZ = Random.Range(-scaleZ, scaleZ);
+        Vector3 randomPosition = new Vector3(randomX, 0, randomZ);
+
+        GameObject newObject = Instantiate(prefabToSpawn.gameObject, randomPosition, baseObject.transform.localRotation);
+        newObject.transform.parent = baseObject.transform;
+        newObject.transform.localPosition = new Vector3(newObject.transform.localPosition.x, 0, newObject.transform.localPosition.z);
     }
 }
